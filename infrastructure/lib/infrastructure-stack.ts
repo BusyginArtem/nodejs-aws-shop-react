1f5e82af27174260759f21f23296639834677bf4
Artem Busyhin <a.busyhin@urancompany.com>
2023-05-29 08:18:58 +0200

    setup infrastructure of aws cdk

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,60 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, "CF-OAI");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const siteBucket = new s3.Bucket(this, "ResourceBucket", {
+      bucketName: "serving-spa-bucket-cdk",
+      websiteIndexDocument: "index.html",
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    siteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["S3:GetObject"],
+        resources: [siteBucket.arnForObjects("*")],
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "CF-distribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: siteBucket,
+              originAccessIdentity: cloudfrontOAI,
+            },
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+              },
+            ],
+          },
+        ],
+      }
+    );
+
+    new s3Deploy.BucketDeployment(this, "JSCC-Bucket-Deployment", {
+      sources: [s3Deploy.Source.asset("../dist")],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
   }
 }
