f0dd47e52d1c897d21e15a017197a219adcdd084
BusyginArtem <artembusygin87@gmail.com>
2023-06-05 10:08:45 +0200

    change service paths

---
+++ infrastructure/bin/infrastructure.ts
@@ -0,0 +1,8 @@
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { InfrastructureStack } from "../lib/infrastructure-stack";
+
+const app = new cdk.App();
+new InfrastructureStack(app, "InfrastructureStack", {
+  env: { region: "eu-west-1" },
+});
